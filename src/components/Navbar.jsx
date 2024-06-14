de81a78ecf6a5e43858e88e28643921e76c16921
Damianmilczarek5 <97959489+Damianmilczarek5@users.noreply.github.com>
2024-06-14 19:04:35 +0200

    updated with links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,23 @@
-import logo from "../assets/DMlogo3.png"
-import {FaLinkedin} from "react-icons/fa";
-import {FaGithub} from "react-icons/fa";
+import React from 'react';
+import logo from "../assets/DMlogo3.png";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
-     <div className="flex flex-shrink-0 items-center">
-     <img className="mx-1 w-13" src={logo} alt="logo"></img>
-     </div>
-     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-     </div>
-     
+      <div className="flex items-center">
+        <img className="mx-1 w-13" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/damian-milczarek-7a04b8161/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/Damianmilczarek5" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
