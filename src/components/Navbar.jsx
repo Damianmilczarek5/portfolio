9d3539bc33ef38f2060f5eb42c4467bfc76cd239
adamsoderstrom <adamsoderstrom@me.com>
2024-07-23 00:26:30 +0200

    migrate to Next.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import logo from "../assets/DMlogo3.png";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
+
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex items-center">
-        <img className="mx-1 w-13" src={logo} alt="logo" />
+        <img className="mx-1 w-13" alt="logo" {...logo} />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a href="https://www.linkedin.com/in/damian-milczarek-7a04b8161/" target="_blank" rel="noopener noreferrer">
--- src/components/Techno.jsx
+++ src/components/Techno.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { RiReactjsLine } from "react-icons/ri"
 import { SiMongodb } from "react-icons/si"
 import { FaNodeJs } from "react-icons/fa"
@@ -8,20 +10,20 @@ import { DiPython } from "react-icons/di";
 
 import { SiCsharp } from "react-icons/si";
 import { SiMysql } from "react-icons/si";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 
 const iconVariants = (duration) => ({
-  initial: {y: -10},
-  animate: {
-    y: [10,-10],
-    transition: {
-      duration: duration,
-      ease: "linear",
-      repeat: Infinity,
-      repeatType: "reverse",
+    initial: { y: -10 },
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        },
     },
-  },
 });
 
 
@@ -29,83 +31,83 @@ const iconVariants = (duration) => ({
 
 
 const Techno = () => {
-  return (
-    <div className="border-b border-neutral-800 pb-24">
-      <motion.h2 
-      whileInView={{opacity: 1, y:0}} 
-      initial={{opacity:0, y: -100}}
-      transition={{duration: 1.5}}
-      className="my-20 text-center text-4xl">Skills</motion.h2>
-      <motion.div whileInView={{opacity: 1, x:0}} 
-      initial={{opacity:0, x: -100}}
-      transition={{duration: 1.5}}
-      className="flex flex-wrap items-center justify-center gap-4">
-        <motion.div 
-        variants={iconVariants(2.5)}
-        initial="initial"
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800 p-4">
-            <RiReactjsLine className="text-7xl text-cyan-400"/>
-        </motion.div>
-        <motion.div 
-        variants={iconVariants(3)}
-        initial="initial"
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiMongodb className="text-7xl text-green-500"/>
+    return (
+        <div className="border-b border-neutral-800 pb-24">
+            <motion.h2
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 1.5 }}
+                className="my-20 text-center text-4xl">Skills</motion.h2>
+            <motion.div whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1.5 }}
+                className="flex flex-wrap items-center justify-center gap-4">
+                <motion.div
+                    variants={iconVariants(2.5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <RiReactjsLine className="text-7xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiMongodb className="text-7xl text-green-500" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaNodeJs className="text-7xl text-green-500" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(2)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaHtml5 className="text-7xl text-orange-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(6)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaCss3Alt className="text-7xl text-blue-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(4)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <IoLogoJavascript className="text-7xl text-yellow-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(1.5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <DiPython className="text-7xl bg-python-gradient" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(2)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiCsharp className="text-7xl text-blue-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(1)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiMysql className="text-7xl " />
+                </motion.div>
             </motion.div>
-            <motion.div 
-            variants={iconVariants(5)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-            <FaNodeJs className="text-7xl text-green-500"/>
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(2)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-            <FaHtml5 className="text-7xl text-orange-400"/>
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(6)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-            <FaCss3Alt className="text-7xl text-blue-400"/>
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(4)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-            <IoLogoJavascript className="text-7xl text-yellow-400"/>
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(1.5)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-            <DiPython className="text-7xl bg-python-gradient"/>
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(2)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiCsharp className="text-7xl text-blue-400"/>
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(1)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiMysql className="text-7xl "/>
-            </motion.div>
-      </motion.div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Techno
